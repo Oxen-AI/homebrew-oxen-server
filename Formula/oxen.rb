89326eff8944ddf01e4c4f65fb46e0bc78213508
Greg Schoeninger <greg.schoeninger@gmail.com>
2022-10-27 13:30:57 -0700

    release v0.2.0

---
+++ Formula/oxen.rb
@@ -0,0 +1,11 @@
+class Oxen < Formula
+  desc "oxen-server 🐂 is the server for oxen to version and manage large machine learning datasets"
+  homepage "https://www.oxen.ai"
+  url "https://github.com/Oxen-AI/oxen-release/releases/download/v0.2.0/oxen-server-mac.tar.gz"
+  sha256 "cf414dc05d061b919250c76c4e8294d53a4401f3f5664dba3ac6b8d041af1505"
+  version "0.2.0"
+
+  def install
+    bin.install "oxen"
+  end
+end
---
+++ README.md
@@ -0,0 +1,10 @@
+# Oxen Server 🚀 🐂
+
+This is the formula for the homebrew release of oxen-server.
+
+More detailed developer documentation can be found [here](https://github.com/Oxen-AI/oxen-release)
+
+```bash
+brew tap Oxen-AI/oxen-server
+brew install oxen-server
+```
