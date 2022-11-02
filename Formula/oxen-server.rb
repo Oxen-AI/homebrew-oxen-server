f1babf7a080a89eef9d0637077cc8c620d9fc565
Greg Schoeninger <greg.schoeninger@gmail.com>
2022-11-02 11:02:48 -0700

    release v0.2.1

--- Formula/oxen-server.rb
+++ Formula/oxen-server.rb
@@ -1,9 +1,9 @@
 class OxenServer < Formula
   desc "oxen-server 🐂 is the server for oxen to version and manage large machine learning datasets"
   homepage "https://www.oxen.ai"
-  url "https://github.com/Oxen-AI/oxen-release/releases/download/v0.2.0/oxen-server-mac.tar.gz"
-  sha256 "70bd2fed694ed6e7f60567a1401c8d78be36186c70b9eef43463c682702d9688"
-  version "0.2.0"
+  url "https://github.com/Oxen-AI/oxen-release/releases/download/v0.2.1/oxen-server-mac.tar.gz"
+  sha256 "8f8b837f2cdad526fcf3847113122be2022316bd6403b9bfac892ac30f2573a9"
+  version "0.2.1"
 
   def install
     bin.install "oxen-server"
