a4ae2e29e8c16a5ffbe773e9dbbc05e8e65036e7
Greg Schoeninger <greg.schoeninger@gmail.com>
2023-01-17 11:11:56 -0800

    bump v0.4.3

--- Formula/oxen-server.rb
+++ Formula/oxen-server.rb
@@ -1,9 +1,9 @@
 class OxenServer < Formula
   desc "oxen-server 🐂 is the server for oxen to version and manage large machine learning datasets"
   homepage "https://www.oxen.ai"
-  url "https://github.com/Oxen-AI/oxen-release/releases/download/v0.4.1/oxen-server-mac.tar.gz"
-  sha256 "64c4b41b680be01fb335373ed108d5d59f28b1a2d1d37b904771c444212e67ea"
-  version "0.4.1"
+  url "https://github.com/Oxen-AI/oxen-release/releases/download/v0.4.3/oxen-server-mac.tar.gz"
+  sha256 "b23bfdddb974050d263dace2c6cb7a7853dad47dc05235d927238f948c1c6856"
+  version "0.4.3"
 
   def install
     bin.install "oxen-server"
