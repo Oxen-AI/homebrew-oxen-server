50b1f5999901aa48b202e37edb628ab024cb3c47
Greg Schoeninger <greg.schoeninger@gmail.com>
2023-01-24 22:10:28 -0800

    fix hash

--- Formula/oxen-server.rb
+++ Formula/oxen-server.rb
@@ -2,7 +2,7 @@ class OxenServer < Formula
   desc "oxen-server 🐂 is the server for oxen to version and manage large machine learning datasets"
   homepage "https://www.oxen.ai"
   url "https://github.com/Oxen-AI/oxen-release/releases/download/v0.4.6/oxen-server-mac-0.4.6.tar.gz"
-  sha256 "ddecf0c390d50f4ff26d5a3a45bf4095f5f0ab50"
+  sha256 "6c62e7e075e0adac6a791cfa7fe9dd34427d91f022ad0430819fb283788377dd"
   version "0.4.6"
 
   def install
