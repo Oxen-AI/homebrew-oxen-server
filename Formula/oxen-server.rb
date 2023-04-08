6b619dd618ffbc3368ab0b40277430dfbdf09b24
Greg Schoeninger <greg.schoeninger@gmail.com>
2023-04-07 18:10:42 -0700

    bump v0.5.2

--- Formula/oxen-server.rb
+++ Formula/oxen-server.rb
@@ -1,23 +1,23 @@
 class OxenServer < Formula
   desc "🐂 oxen-server is the server for oxen to version and manage large machine learning datasets"
   homepage "https://www.oxen.ai"
-  version "0.5.1"
+  version "0.5.2"
 
   on_macos do
     if MacOS.version >= :ventura # 13
       if Hardware::CPU.arm?
-        url "https://github.com/Oxen-AI/Oxen/releases/download/v0.5.1/oxen-server-mac-0.5.1.tar.gz"
-        sha256 "f0890773336bdaadb5795a82fd95c6fd8579f85f2977f7ab295160b28a632ee6"
+        url "https://github.com/Oxen-AI/Oxen/releases/download/v0.5.2/oxen-server-mac-0.5.2.tar.gz"
+        sha256 "0d8148232de11746a3b461c1fbb986a200edc2c931f25bf94f744c3005c1170d"
       elsif Hardware::CPU.intel?
-        url "https://github.com/Oxen-AI/Oxen/releases/download/v0.5.1/oxen-server-mac-x86_64-0.5.1.tar.gz"
-        sha256 "b8fb2206ba6187d482d8840aba64cdff25578bc66ad1e165a75df076a5b772e9"
+        url "https://github.com/Oxen-AI/Oxen/releases/download/v0.5.2/oxen-server-mac-x86_64-0.5.2.tar.gz"
+        sha256 "50c928de8aba617c5a63d7364cf1de891df0ab0a3e1a9882cdfae74ee60822f5"
       end
     elsif MacOS.version >= :monterey # 12
-      url "https://github.com/Oxen-AI/Oxen/releases/download/v0.5.1/oxen-server-mac-12.0-0.5.1.tar.gz"
-      sha256 "0aeb6644e0de36fe5c732b404581bbe9e847d8dbf8ec7cf3fb3a6d009dfac5c8"
+      url "https://github.com/Oxen-AI/Oxen/releases/download/v0.5.2/oxen-server-mac-12.0-0.5.2.tar.gz"
+      sha256 "64fd8b5f6467a4ef55322fa0fff0b2539099acc11582d5f49ea5b3737fb62635"
     elsif MacOS.version >= :big_sur # 11
-      url "https://github.com/Oxen-AI/Oxen/releases/download/v0.5.1/oxen-server-mac-11.0-0.5.1.tar.gz"
-      sha256 "13cd87b728a9429897531711fc15e636c52ee157e699306dd3a8d0bfa9a30dec"
+      url "https://github.com/Oxen-AI/Oxen/releases/download/v0.5.2/oxen-server-mac-11.0-0.5.2.tar.gz"
+      sha256 "0a92dd692b718f97e9d9b8e6813fe8490850fef9969560bed9e4ffcf72509177"
     end
   end
 
